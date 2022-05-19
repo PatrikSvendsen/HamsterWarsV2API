ca391ac1623b3934d49feccb745d17fa0174f66e
Patrik Svendsen <patrik.svendsen@outlook.com>
2022-05-19 19:41:33 +0200

    Chapter01 - Done -Fixed Folder-layout and main project created Chapter02 - Done -Fixed Logger and editorsettings Chapter03 - NOT DONE -Added models. Stopped at 3.2-Creating Models

---
+++ Entities/Models/Hamster.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Hamster
+{
+    [Key]
+    [Column("HamsterId")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name field is required")]
+    [MaxLength(20)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Age field is required")]
+    public int Age { get; set; }
+
+    [MaxLength(20)]
+    [Required(ErrorMessage = "Food field is required")]
+    public string FavFood { get; set; } = string.Empty;
+
+    [MaxLength(20)]
+    [Required(ErrorMessage = "Loves field is required")]
+    public string Loves { get; set; } = string.Empty;
+
+    public string ImgName { get; set; } = string.Empty;
+    public int Wins { get; set; } = 0;
+    public int Defeats { get; set; } = 0;
+    public int Games { get; set; } = 0;
+
+
+    [ForeignKey(nameof(Match))]
+    public int MatchId { get; set; }
+    public Match? Match { get; set; }
+}
---
+++ Entities/Models/Match.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Match
+{
+    [Key]
+    [Column("MatchId")]
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(Hamster))]
+    public int WinnerId { get; set; }
+
+    [ForeignKey(nameof(Hamster))]
+    public int LoserId { get; set; }
+}
